ac83ac529e1bbd1f563234a6ec7809a80b0076f2
Yuldashev Sarvar <yuldashev6267@gmail.com>
2021-04-07 14:58:43 +0500

    added cors

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 const express = require("express");
-const app = express();
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const compression = require("compression");
+const cors = require("cors");
+
 const userReqRouter = require("./routes/userReqRouter");
 const adminRouter = require("./routes/adminRouter");
 const shopsRouter = require("./routes/shopsRouter");
 
+const app = express();
+app.enable("trust proxy");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.options("*", cors());
+app.use(helmet());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+app.use(xss());
+
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(compression());
+
 app.use("/api", userReqRouter);
 app.use("/api", adminRouter);
 app.use("/api", shopsRouter);
-
-app.use((req, res, next) => {
-  //to allow cross domain requests to send cookie information.
-  res.header("Access-Control-Allow-Credentials", true);
-
-  // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
-  res.header("Access-Control-Allow-Origin", "localhost:3000");
-  res.header("Access-Control-Allow-Origin", "*");
-
-  // list of methods that are supported by the server
-  res.header("Access-Control-Allow-Methods", "OPTIONS,GET,PUT,POST,DELETE");
-
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept,authorization"
-  );
-  next();
-});
 
 module.exports = app;
